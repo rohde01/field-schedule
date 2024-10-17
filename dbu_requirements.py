f752a61f67b12ba94f674cae4ac28a401345b877
Carl-Emil Rohde <rohdee@icloud.com>
2024-10-17 18:53:11 +0200

    modified data-strcture to accomedate a list of constraints

--- dbu_requirements.py
+++ dbu_requirements.py
@@ -1,19 +1,19 @@
 # 5-star constraints
 def get_5_star_constraints():
-    return {
-        'U19': {'required_size': 'full', 'sessions': 2},
-        'U17': {'required_size': 'full', 'sessions': 2},
-        'U15': {'required_size': 'full', 'sessions': 1},
-        'U14': {'required_size': 'half', 'sessions': 3},
-        'U13': {'required_size': 'half', 'sessions': 3},
-        'U10': {'required_size': 'quarter', 'sessions': 2},
-        'U11': {'required_size': 'quarter', 'sessions': 2},
-        'U12': {'required_size': 'quarter', 'sessions': 2},
-        'U13-girl': {'required_size': 'half', 'sessions': 3},
-        'U14-girl': {'required_size': 'half', 'sessions': 3},
-        'U16-girl': {'required_size': 'full', 'sessions': 2},
-        'U19-girl': {'required_size': 'full', 'sessions': 2},
-    }
+    return [
+        {'year': 'U19', 'required_size': 'full', 'sessions': 2},
+        {'year': 'U17', 'required_size': 'full', 'sessions': 2},
+        {'year': 'U15', 'required_size': 'full', 'sessions': 1}, {'year': 'U15', 'required_size': 'half', 'sessions': 1},
+        {'year': 'U14', 'required_size': 'half', 'sessions': 3},
+        {'year': 'U13', 'required_size': 'half', 'sessions': 3},
+        {'year': 'U10', 'required_size': 'quarter', 'sessions': 2},
+        {'year': 'U11', 'required_size': 'quarter', 'sessions': 2},
+        {'year': 'U12', 'required_size': 'quarter', 'sessions': 2},
+        {'year': 'U13-girl', 'required_size': 'half', 'sessions': 3},
+        {'year': 'U14-girl', 'required_size': 'half', 'sessions': 3},
+        {'year': 'U16-girl', 'required_size': 'full', 'sessions': 2},
+        {'year': 'U19-girl', 'required_size': 'full', 'sessions': 2},
+    ]
 
 # 4-star constraints
 def get_4_star_constraints():
