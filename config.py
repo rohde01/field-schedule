81855e1ff3c9f4fd5276061460f6c5cace8eb9b2
Carl-Emil Rohde <rohdee@icloud.com>
2024-10-20 20:25:25 +0200

    Remove obsolete configuration and data files for scheduling algorithm

--- config.py
+++
@@ -1,6 +0,0 @@
-# Filename: config.py
-# Description: Configuration file for the scheduling algorithm.
-
-# Constants
-SIZE_TO_SUBFIELDS = {'full': 4, 'half': 2, 'quarter': 1, 'any': 1}
-SESSION_LENGTH_MINUTES = 15
