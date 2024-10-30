e5abbae46799e980ded5f2d0ae15cc83dfce5622
Carl-Emil Rohde <rohdee@icloud.com>
2024-10-30 22:39:29 +0100

    Enforce only 1 session per day per team

--- model.py
+++ model.py
@@ -16,13 +16,16 @@ def create_variables(model, teams, constraints, time_slots, size_to_combos):
 
     # Map time slots to global indices
     global_time_slots = []
-    time_slot_indices = {}
     idx = 0
+    idx_to_time = {}
+    idx_to_day = []
+    day_to_idx = {}
     for day in time_slots:
-        time_slot_indices[day] = {}
+        day_to_idx[day] = idx  # Map day to index
         for t, slot_time in enumerate(time_slots[day]):
-            time_slot_indices[day][t] = idx
+            idx_to_time[idx] = (day, t)
             global_time_slots.append((day, t))
+            idx_to_day.append(list(time_slots.keys()).index(day))
             idx += 1
     num_global_slots = idx
 
@@ -64,6 +67,11 @@ def create_variables(model, teams, constraints, time_slots, size_to_combos):
                 else:
                     assigned_combo = model.NewConstant(0)
 
+                # Create day variable
+                day_var = model.NewIntVar(0, len(time_slots) - 1, f'day_{team_name}_{idx_constraint}_{session_idx}')
+                # Link start_var to day_var using AddElement
+                model.AddElement(start_var, idx_to_day, day_var)
+
                 interval_vars[team_name][idx_constraint].append({
                     'start': start_var,
                     'end': end_var,
@@ -72,10 +80,20 @@ def create_variables(model, teams, constraints, time_slots, size_to_combos):
                     'constraint': constraint,
                     'assigned_combo': assigned_combo,
                     'possible_combos': possible_combos,
-                    'combo_indices': combo_indices
+                    'combo_indices': combo_indices,
+                    'day_var': day_var
                 })
 
                 assigned_fields[team_name][idx_constraint].append(assigned_combo)
+
+    # After all variables are created, add the AllDifferent constraints for each team
+    for team_name in interval_vars:
+        team_day_vars = []
+        for idx_constraint in interval_vars[team_name]:
+            sessions = interval_vars[team_name][idx_constraint]
+            for session in sessions:
+                team_day_vars.append(session['day_var'])
+        model.AddAllDifferent(team_day_vars)
 
     return interval_vars, assigned_fields, global_time_slots
 
