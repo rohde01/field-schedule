0dfa9854c5ab68fda79e19680f6a088c9fc6f851
Carl-Emil Rohde <rohdee@icloud.com>
2024-11-18 17:33:51 +0100

    introduced the new optional entries for partial space and time

--- test_data.py
+++ test_data.py
@@ -10,12 +10,12 @@ def get_teams():
     return [
         {'team_id': 1, 'name': 'U19A', 'year': 'U19'},
         {'team_id': 2, 'name': 'U14A', 'year': 'U14'},
-        {'team_id': 3, 'name': 'Y15A', 'year': 'U15'},
-        {'team_id': 4, 'name': 'U10A', 'year': 'U10'},
-        {'team_id': 5, 'name': 'U11A', 'year': 'U11'},
-        {'team_id': 6, 'name': 'U13A', 'year': 'U13'},
-        {'team_id': 7, 'name': 'girl', 'year': 'U19-girl'},
-        {'team_id': 8, 'name': 'child', 'year': 'U17-girl'},
+        #{'team_id': 3, 'name': 'Y15A', 'year': 'U15'},
+        #{'team_id': 4, 'name': 'U10A', 'year': 'U10'},
+        #{'team_id': 5, 'name': 'U11A', 'year': 'U11'},
+        #{'team_id': 6, 'name': 'U13A', 'year': 'U13'},
+        #{'team_id': 7, 'name': 'girl', 'year': 'U19-girl'},
+        #{'team_id': 8, 'name': 'child', 'year': 'U17-girl'},
     ]
 
 def get_fields():
@@ -65,18 +65,17 @@ def get_fields():
 def get_constraints():
     """Returns a list of constraints dictionaries with 'team_id' instead of 'year'."""
     return [
+        {'team_id': 1, 'required_size': '11v11', 'subfield_type': 'half', 'sessions': 5, 'length': 8,
+        'partial_ses_space': 'full', 'partial_ses_time': 4},
 
-        {'team_id': 1, 'required_size': '8v8', 'subfield_type': 'quarter', 'sessions': 1, 'length': 4, 'start_time': '16:30'},
-        {'team_id': 1, 'required_size': '11v11', 'subfield_type': 'full', 'sessions': 1, 'length': 4},
+        {'team_id': 2, 'required_cost': 250, 'sessions': 3, 'length': 4,
+         'partial_ses_space': 500, 'partial_ses_time': 2},
 
-        {'team_id': 2, 'required_cost': 500, 'sessions': 1, 'length': 4, 'start_time': '17:00'},
-        {'team_id': 2, 'required_cost': 250, 'sessions': 2, 'length': 7},
+        #{'team_id': 3, 'required_cost': 500, 'sessions': 1, 'length': 4},
+        #{'team_id': 6, 'required_size': '11v11', 'subfield_type': 'quarter', 'sessions': 1, 'length': 2},
+        #{'team_id': 4, 'required_size': '5v5', 'subfield_type': 'full', 'sessions': 1, 'length': 4},
+        #{'team_id': 5, 'required_cost': 1000, 'sessions': 1, 'length': 4},
 
-        {'team_id': 3, 'required_cost': 500, 'sessions': 1, 'length': 4},
-        {'team_id': 6, 'required_size': '11v11', 'subfield_type': 'quarter', 'sessions': 1, 'length': 2},
-        {'team_id': 4, 'required_size': '5v5', 'subfield_type': 'full', 'sessions': 1, 'length': 4},
-        {'team_id': 5, 'required_cost': 1000, 'sessions': 1, 'length': 4},
-
-        {'team_id': 7, 'required_cost': 500, 'sessions': 3, 'length': 4},
-        {'team_id': 8, 'required_cost': 500, 'sessions': 4, 'length': 4},
+        #{'team_id': 7, 'required_cost': 500, 'sessions': 3, 'length': 4},
+        #{'team_id': 8, 'required_cost': 500, 'sessions': 4, 'length': 4},
     ]
