d6704d1674c38a96485aac49e4eec50a2b83c59f
Carl-Emil Rohde <cer@nationalbanken.com>
2025-04-09 14:12:29 +0200

    deleted by mistake

---
+++ frontend/src/routes/+layout.ts
@@ -0,0 +1,49 @@
+import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import type { LayoutLoad } from './$types'
+
+export const load: LayoutLoad = async (event) => {
+  // Declare a dependency so the layout can be invalidated, for example, on
+  // session refresh.
+  event.depends('supabase:auth')
+
+  const supabase = isBrowser()
+    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: {
+          fetch: event.fetch,
+        },
+      })
+    : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: {
+          fetch: event.fetch,
+        },
+        cookies: {
+          getAll() {
+            return event.data.cookies
+          },
+        },
+      })
+
+  // It's fine to use `getSession` here, because on the client, `getSession` is
+  // safe, and on the server, it reads `session` from the `LayoutData`, which
+  // safely checked the session using `safeGetSession`.
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return { 
+    session, 
+    supabase, 
+    user,
+    cookies: {},
+    teams: event.data.teams || [],
+    facilities: event.data.facilities || [],
+    fields: event.data.fields || [],
+    schedules: event.data.schedules || [],
+    events: event.data.events || [] 
+  }
+}
